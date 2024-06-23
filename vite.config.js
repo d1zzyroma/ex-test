1f0b255b829617f52e6ef3285f1fb54ff3fb1b4d
d1zzyroma <nataliawologin@gmail.com>
2024-06-23 21:39:11 +0200

    321

--- vite.config.js
+++ vite.config.js
@@ -3,28 +3,24 @@ import glob from 'glob';
 import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
 
-export default defineConfig(({ command }) => {
-  return {
-    define: {
-      [command === 'serve' ? 'global' : '_global']: {},
-    },
-    root: 'src',
-    build: {
-      sourcemap: true,
-
-      rollupOptions: {
-        input: glob.sync('./src/*.html'),
-        output: {
-          manualChunks(id) {
-            if (id.includes('node_modules')) {
-              return 'vendor';
-            }
-          },
-          entryFileNames: 'commonHelpers.js',
+export default defineConfig(({ command }) => ({
+  define: {
+    [command === 'serve' ? 'global' : '_global']: {},
+  },
+  build: {
+    sourcemap: true,
+    rollupOptions: {
+      input: glob.sync('./*.html'),
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
         },
+        entryFileNames: 'commonHelpers.js',
       },
-      outDir: '../dist',
     },
-    plugins: [injectHTML(), FullReload(['./src/**/**.html'])],
-  };
-});
+    outDir: 'dist',
+  },
+  plugins: [injectHTML(), FullReload(['./*.html'])],
+}));
